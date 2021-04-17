352ee2d3d62d40e2502fdc6913573cfe1b7afd03
Shivkumar Ople <sople@redhat.com>
2021-04-17 17:21:15 +0530

    main main

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
-import fmt	
+import (
+	"fmt"
+)
 
-func main()  {
-	
+var y = 25
+
+var z int
+
+//Declare a variable with the identifier "z" if of TYPE int
+
+//ASSINGS the ZERO VALUE of TYPE int to "z"
+
+//false for booleans, 0 for integers. 0.0 for floats, "" for strings, and nil for pointers, functions, interfaces, slices, channels and maps.
+func main() {
+
 	fmt.Println("Hello Shivkumar")
 
-	fmt.Println("Working with git")
+	fmt.Println("")
 
-   fmt.Println("Nope at all")
+	fmt.Println(y)
+
 }
-
